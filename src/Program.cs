5fa5b50c7f542c7e1218949372183ccc31495948
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-10-20 01:12:41 +0200

    Add solution and project files

---
+++ src/Program.cs
@@ -0,0 +1,19 @@
+using FifteenToOne.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+app.UseStaticFiles();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+app.Run();
